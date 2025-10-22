ce6e9b3e0085bfd480e930b28c6e618500d5c60c
Magnus <magnus.danner@outlook.de>
2025-10-22 13:52:02 +0200

    Added Desc-Fuc, Added Start-Structures

---
+++ src/models/SEB.jl
@@ -0,0 +1,39 @@
+using RDKitMinimalLib
+function M(descs::Dict)
+    return get(descs,"exactmw",0)
+end
+function R(descs::Dict)
+    return !(get(descs,"NumRings",0) == 0)
+end
+function r_het(descs::Dict)
+    return (get(descs,"NumHeteroatoms",0)/get(descs,"NumHeavyAtoms",0))
+end
+function r_acc(descs::Dict)
+    return (get(descs,"NumHBA",0)/get(descs,"NumHeavyAtoms",0))
+end
+function r_don(descs::Dict)
+    return (get(descs,"NumHBD",0)/get(descs,"NumHeavyAtoms",0))
+end
+function r_hal(SMILES::String,descs::Dict)
+    mol = get_mol(SMILES)
+    N_h=0
+    for h in ["F", "Cl", "Br", "I"] # , "At", "Ts"]
+       qmol = get_qmol(h)
+       N_h += length(get_substruct_matches(mol, qmol))
+    end
+    return N_h/get(descs,"NumHeavyAtoms",0)
+end
+
+
+struct MOLE_DESC
+    X_j::Vector
+    X_i::Vector
+
+end
+function SEB(SMILE_i::String,SMILE_j::String)
+    mol_i,mol_j = get_mol(SMILE_i), get_mol(SMILE_j)
+    desc_i,desc_j = get_descriptors(mol_i), get_descriptors(mol_j)
+    X_i_ini=[M(desc_i);R(desc_i);r_het(desc_i);r_hal(SMILE_i,desc_i);r_acc(desc_i);r_don(desc_i)]
+    X_j_ini=[M(desc_j);R(desc_j);r_het(desc_j);r_hal(SMILE_j,desc_j);r_acc(desc_j);r_don(desc_j)]
+    return MOLE_DESC(X_j_ini,X_i_ini)
+end
