a2b27f62418816bdac657224d3b573e5051ec938
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-02-12 09:35:06 +0100

    [database] move data to folder database + add const DB_PATH + pysical constants + remove properties db

--- src/MLPROP.jl
+++ src/MLPROP.jl
@@ -4,6 +4,14 @@ using Clapeyron, Lux, ConcreteStructs, ChemBERTa, LinearAlgebra, DelimitedFiles,
 
 const CL = Clapeyron
 
+const kB = 1.380649e-23
+const NA = 6.02214076e23
+const R = kB*NA
+
+const DB_PATH = normpath(Base.pkgdir(MLPROP),"database")
+
+BERT = nothing
+
 # Models
 include("HANNA_legacy.jl")
 
