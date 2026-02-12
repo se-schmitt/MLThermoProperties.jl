c9ca2fc7cf20f43cb42d399a54d8c7a168b96eea
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-02-12 15:40:16 +0100

    [feat] add show method for MLPROP models

--- src/models/models.jl
+++ src/models/models.jl
@@ -2,3 +2,5 @@ include("utils.jl")
 
 include("grappa.jl")
 include("HANNA/hanna.jl")
+
+MLPROP_MODELS = Union{GRAPPA, ogHANNA}
--- src/models/utils.jl
+++ src/models/utils.jl
@@ -14,3 +14,16 @@ function load_scaler(path::String, ::Type{Scaler}; T=Float64)
     @load joinpath(DB_PATH, path) μ σ
     return Scaler(T.(μ), T.(σ))
 end
+
+# Show method
+function Base.show(io::IO, mime::MIME"text/plain", model::MLPROP_MODELS)
+    print(io, nameof(typeof(model)))
+    length(model) == 1 && println(io, " with 1 component:")
+    length(model) > 1 && println(io, " with ", length(model), " components:")
+    CL.show_pairs(io,CL.component_list(model))
+    
+    CL.show_info(io,model)
+    CL.show_params(io,model)
+    CL.show_reference_state(io,model)
+    CL.may_show_references(io,model)
+end
